2ebff97bc146d7fdad60bd9d949ba3e455129aef
Niklas Dahlman <nd@sql8r.net>
2018-12-02 17:47:22 +0100

    day2

--- AoC.Day2/Services/CalculationService.cs
+++ AoC.Day2/Services/CalculationService.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Linq;
 using AoC.Day2.Interfaces;
 
 namespace AoC.Day2.Services
@@ -23,20 +24,68 @@ namespace AoC.Day2.Services
 
         private int CheckSum(string[] ids)
         {
-            return 0;
+            int accTwos = 0;
+            int accThrees = 0;
+
+            foreach (var id in ids)
+            {
+                var twos = id.GroupBy(x => x)
+                    .Where(g => g.Count() == 2)
+                    .Select(y => y.Key).Any();
+                accTwos += twos?1:0;
+
+                var threes = id.GroupBy(x => x)
+                    .Where(g => g.Count() == 3)
+                    .Select(y => y.Key).Any();
+                accThrees += threes?1:0;
+
+            }
+
+            return accTwos * accThrees;
         }
-
-
-
-
-
-
-
-
 
         public string DoPart2()
         {
-            return "";
+            var lines = File.ReadAllLines($"Files\\{_day}.txt");
+
+            var diffComm = FindOneDiffers(lines);
+
+            return diffComm;
+        }
+
+        private string FindOneDiffers(string[] ids)
+        {
+            var simis = ids.Where(id => ids.Any(i => Similar(id, i))).ToArray();
+
+            var over = simis[0].Zip(simis[1], InterSec).Where(c => c != ' ');
+
+            return new string(over.ToArray());
+        }
+
+        private char InterSec(char id1, char id2)
+        {
+            if (id1 == id2)
+                return id1;
+
+            return ' ';
+        }
+
+        private bool Similar(string id1, string id2)
+        {
+            int differs=0;
+            for (int i = 0; i < id1.Length; i++)
+            {
+                if (id1[i] != id2[i])
+                {
+                    differs++;
+                }
+            }
+
+            if (differs == 1)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
