2fbd7db069e47b71c44cc953470fd9dfa74632c4
Niklas Dahlman <nd@sql8r.net>
2018-12-03 23:22:33 +0100

    day3

---
+++ AoC.Day3/Models/Claim.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace AoC.Day3.Models
+{
+    public class Claim
+    {
+        public int Id { get; set; }
+        public Point Position { get; set; }
+        public Size Area { get; set; }
+    }
+}
--- AoC.Day3/Services/CalculationService.cs
+++ AoC.Day3/Services/CalculationService.cs
@@ -1,19 +1,44 @@
-﻿using System.IO;
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
 using AoC.Day3.Interfaces;
+using AoC.Day3.Models;
 
 namespace AoC.Day3.Services
 {
     public class CalculationService : ICalculationService
     {
-        private readonly string[] _lines;
+        private readonly Claim[] _claims;
 
         public CalculationService(string day)
         {
-            _lines = File.ReadAllLines($"Files\\{day}.txt");
+            string[] lines = File.ReadAllLines($"Files\\{day}.txt");
+            _claims = lines.Select(Parse).ToArray();
         }
+
+        private Claim Parse(string line)
+        {
+            var parts = line.Split(' ');
+
+            return new Claim
+            {
+                Id = int.Parse(parts[0].Replace("#", "")),
+                Position = new Point(int.Parse(parts[2].Split(",")[0]), int.Parse(parts[2].Split(",")[1].Replace(":", ""))),
+                Area = new Size(int.Parse(parts[3].Split("x")[0]), int.Parse(parts[3].Split("x")[1]))
+            };
+        }
+
+        /* #1 @ 1,3: 4x4
+           #2 @ 3,1: 4x4
+           #3 @ 5,5: 2x2 */
 
         public string DoPart1()
         {
+            foreach (var c in _claims)
+            {
+
+            }
             return "";
         }
 
