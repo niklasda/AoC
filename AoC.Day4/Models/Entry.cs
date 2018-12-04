a3248491ca4baab5c7075fefc11405df275c1fa4
Niklas Dahlman <nd@sql8r.net>
2018-12-04 20:41:07 +0100

    day4

---
+++ AoC.Day4/Models/Entry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AoC.Day4.Models
+{
+    public class Entry
+    {
+        public Entry()
+        {
+            Events=new List<Event>();
+        }
+
+        public DateTime EntryDate { get; set; }
+        public int Id { get; set; }
+        public IList<Event> Events { get; set; }
+    }
+
+    public class Event
+    {
+        public DateTime EventTime { get; set; }
+        public EventType EventType { get; set; }
+    }
+
+    public enum EventType
+    {
+        FallAsleep,
+        WakeUp
+    };
+}
--- AoC.Day4/Services/CalculationService.cs
+++ AoC.Day4/Services/CalculationService.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Drawing;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using AoC.Day4.Interfaces;
@@ -9,35 +9,86 @@ namespace AoC.Day4.Services
 {
     public class CalculationService : ICalculationService
     {
-        private readonly Claim[] _claims;
+        private readonly IList<Entry> _entries;
 
         public CalculationService(string day)
         {
             string[] lines = File.ReadAllLines($"Files\\{day}.txt");
-            _claims = lines.Select(Parse).ToArray();
+
+            _entries = Parse(lines);
         }
 
-        private Claim Parse(string line)
+        private IList<Entry> Parse(string[] lines)
         {
-            var parts = line.Split(' ');
+            IList<Entry> entries = new List<Entry>();
 
-            var c = new Claim
+            foreach (var line in lines)
             {
-                Id = int.Parse(parts[0].Replace("#", "")),
-                Position = new Point(int.Parse(parts[2].Split(",")[0]), int.Parse(parts[2].Split(",")[1].Replace(":", ""))),
-                Area = new Size(int.Parse(parts[3].Split("x")[0]), int.Parse(parts[3].Split("x")[1])),
-            };
-            c.Rect = new Rectangle(c.Position, c.Area);
-            return c;
+                var parts = line.Split(']');
+
+                string dateString = parts[0].Replace("[", "");
+
+                var dateTime = DateTime.ParseExact(dateString, "yyyy-MM-dd HH:mm", null);
+                DateTime date;
+                if (dateTime.Hour >= 23)
+                {
+                    date = dateTime.AddHours(1).Date;
+                }
+                else
+                {
+                    date = dateTime.Date;
+                }
+
+                var entry = entries.FirstOrDefault(e => e.EntryDate.Equals(date));
+                if (entry == null)
+                {
+                    string text = parts[1].Trim();
+                    string guardId = text.Replace("Guard #", "").Replace(" begins shift", "");
+
+                    var e = new Entry();
+                    e.EntryDate = date;
+                    e.Id = int.Parse(guardId);
+
+                    entries.Add(e);
+                }
+                else
+                {
+                    string text = parts[1].Trim();
+                    if (text.StartsWith("falls", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        entry.Events.Add(new Event { EventTime = dateTime, EventType = EventType.FallAsleep });
+                    }
+                    else if (text.StartsWith("wakes", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        entry.Events.Add(new Event { EventTime = dateTime, EventType = EventType.WakeUp });
+                    }
+                }
+            }
+
+            return entries;
         }
 
-        /* #1 @ 1,3: 4x4
-           #2 @ 3,1: 4x4
-           #3 @ 5,5: 2x2 */
+        /* [1518-11-01 00:00] Guard #10 begins shift
+           [1518-11-01 00:05] falls asleep
+           [1518-11-01 00:25] wakes up
+           [1518-11-01 00:30] falls asleep
+           [1518-11-01 00:55] wakes up
+           [1518-11-01 23:58] Guard #99 begins shift
+           [1518-11-02 00:40] falls asleep
+           [1518-11-02 00:50] wakes up
+           [1518-11-03 00:05] Guard #10 begins shift
+           [1518-11-03 00:24] falls asleep
+           [1518-11-03 00:29] wakes up
+           [1518-11-04 00:02] Guard #99 begins shift
+           [1518-11-04 00:36] falls asleep
+           [1518-11-04 00:46] wakes up
+           [1518-11-05 00:03] Guard #99 begins shift
+           [1518-11-05 00:45] falls asleep
+           [1518-11-05 00:55] wakes up */
 
         public string DoPart1()
         {
-            foreach (var c in _claims)
+            foreach (var e in _entries)
             {
 
             }
