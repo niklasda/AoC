2e4925f553679f1c4720bdd22dda7c52d7c2f7d1
Niklas Dahlman <nd@sql8r.net>
2018-12-06 11:14:09 +0100

    day6

---
+++ AoC.Day6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AoC.Common.Tools;
+using AoC.Day6.Services;
+
+namespace AoC.Day6
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            const string day = "day6";
+            Console.WriteLine(day);
+
+            CalcRunner.Run(new CalculationService(day));
+        }
+    }
+}
---
+++ AoC.Day6/Services/CalculationService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using AoC.Common.Interfaces;
+
+namespace AoC.Day6.Services
+{
+    public class CalculationService : ICalculationService
+    {
+        private readonly string _line;
+
+        public CalculationService(string day)
+        {
+            _line = File.ReadAllText($"Files\\{day}.txt");
+        }
+
+        public string DoPart1()
+        {
+            StringBuilder sb = new StringBuilder(_line);
+            Console.WriteLine($"Original: {sb.Length}");
+
+
+            Console.WriteLine($"Final: {sb.Length}");
+
+            return sb.Length.ToString();
+        }
+
+        public string DoPart2()
+        {
+            var results = new Dictionary<string, int>();
+
+
+            return results.Values.Min().ToString();
+        }
+    }
+}
